409818586788e1bfbbea18e87a768b80a0fbcd61
Andrew Stock <totally@funoninter.net>
2013-04-30 23:55:27 -0400

    adding location to admin visit list

--- aroundtown/locations/admin.py
+++ aroundtown/locations/admin.py
@@ -5,7 +5,7 @@ class LocationAdmin(admin.ModelAdmin):
 	list_display = ['name', 'formatted_address', 'last_visited_date']
 
 class VisitAdmin(admin.ModelAdmin):
-	list_display = ['visit_date', 'rating']
+	list_display = ['visit_date', 'location', 'rating']
 	list_filter = ['location', 'visit_date', 'rating']
 
 admin.site.register(Location, LocationAdmin)
